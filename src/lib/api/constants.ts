d42e935db84a7389f826d0d9d1ff638a2e445ebb
LeeByeongMuk <mansogod@gmail.com>
2025-02-15 12:17:42 +0900

    Update constants.ts

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -1,6 +1,5 @@
 import path from 'path';
 
-export const BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? '';
 
 export const POSTS_DIR = path.join(process.cwd(), 'src/posts');
