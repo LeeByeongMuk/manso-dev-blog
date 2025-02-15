a9dc844d3284d123644b9b4a2e70982325ceee22
LeeByeongMuk <mansogod@gmail.com>
2025-02-15 19:50:53 +0900

    Update constants.ts

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -1,8 +1,21 @@
 import path from 'path';
 
-export const BASE_URL = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
-  : (process.env.NEXT_PUBLIC_BASE_URL ??
-    (process.env.CI ? 'http://127.0.0.1:3000' : 'http://localhost:3000'));
+function getBaseURL() {
+  if (process.env.VERCEL_URL) {
+    return `https://${process.env.VERCEL_URL}`;
+  }
+
+  if (process.env.CI) {
+    return 'http://127.0.0.1:3000';
+  }
+
+  if (process.env.NEXT_PUBLIC_BASE_URL) {
+    return process.env.NEXT_PUBLIC_BASE_URL;
+  }
+
+  return 'http://localhost:3000';
+}
+
+export const BASE_URL = getBaseURL();
 
 export const POSTS_DIR = path.join(process.cwd(), 'src/posts');
