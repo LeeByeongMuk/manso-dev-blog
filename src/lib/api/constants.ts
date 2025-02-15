4d167b072b36c3f57f59387586ebccde67222385
LeeByeongMuk <mansogod@gmail.com>
2025-02-15 12:42:09 +0900

    chore: fix build error

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -1,5 +1,9 @@
 import path from 'path';
 
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL ?? '';
+export const BASE_URL =
+  process.env.NEXT_PUBLIC_BASE_URL ??
+  (process.env.NEXT_PUBLIC_VERCEL_URL
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000');
 
 export const POSTS_DIR = path.join(process.cwd(), 'src/posts');
