6cfe9ff7a2eb9fd83147b8dd162733187aeb5c34
LeeByeongMuk <mansogod@gmail.com>
2025-02-15 18:19:04 +0900

    fix: build error

--- src/lib/api/constants.ts
+++ src/lib/api/constants.ts
@@ -2,6 +2,7 @@ import path from 'path';
 
 export const BASE_URL = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : (process.env.NEXT_PUBLIC_BASE_URL ?? 'http://localhost:3000');
+  : (process.env.NEXT_PUBLIC_BASE_URL ??
+    (process.env.CI ? 'http://127.0.0.1:3000' : 'http://localhost:3000'));
 
 export const POSTS_DIR = path.join(process.cwd(), 'src/posts');
