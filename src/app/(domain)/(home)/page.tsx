77f1676539d9e7a53866631f0de2a6a8134e2d1e
LeeByeongMuk <mansogod@gmail.com>
2025-02-16 23:11:42 +0900

    fix: build

--- src/app/(domain)/(home)/page.tsx
+++ src/app/(domain)/(home)/page.tsx
@@ -2,24 +2,26 @@
 
 import { Suspense, useState } from 'react';
 
-import PostList from '@domains/post/list/_components/PostList';
-import PostListSkeleton from '@domains/post/list/_components/PostList.Skeleton';
+import PostListFilters from '@domains/post/list/_components/PostListFilters';
+import PostListFiltersSkeleton from '@domains/post/list/_components/PostListFilters.Skeleton';
 
 export default function Home() {
-  const [selectedCategory] = useState<string>('all');
+  const [selectedCategory, setSelectedCategory] = useState<string>('all');
 
   return (
     <section className="mx-auto max-w-3xl bg-background px-6 py-10 text-foreground">
       <h2 className="mb-6 text-3xl font-bold text-foreground">Posts</h2>
 
-      {/*<PostListFiltersContainer*/}
-      {/*  selectedCategory={selectedCategory}*/}
-      {/*  setSelectedCategory={setSelectedCategory}*/}
-      {/*/>*/}
-
-      <Suspense fallback={<PostListSkeleton />}>
-        <PostList category={selectedCategory} />
+      <Suspense fallback={<PostListFiltersSkeleton />}>
+        <PostListFilters
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
       </Suspense>
+
+      {/*<Suspense fallback={<PostListSkeleton />}>*/}
+      {/*  <PostList category={selectedCategory} />*/}
+      {/*</Suspense>*/}
     </section>
   );
 }
