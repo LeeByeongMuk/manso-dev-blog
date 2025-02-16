699bf42ece7aa9ab2c0a42fe553450a04922b97e
LeeByeongMuk <mansogod@gmail.com>
2025-02-16 23:13:40 +0900

    Update page.tsx

--- src/app/(domain)/(home)/page.tsx
+++ src/app/(domain)/(home)/page.tsx
@@ -3,7 +3,6 @@
 import { Suspense, useState } from 'react';
 
 import PostListFilters from '@domains/post/list/_components/PostListFilters';
-import PostListFiltersSkeleton from '@domains/post/list/_components/PostListFilters.Skeleton';
 
 export default function Home() {
   const [selectedCategory, setSelectedCategory] = useState<string>('all');
@@ -12,7 +11,7 @@ export default function Home() {
     <section className="mx-auto max-w-3xl bg-background px-6 py-10 text-foreground">
       <h2 className="mb-6 text-3xl font-bold text-foreground">Posts</h2>
 
-      <Suspense fallback={<PostListFiltersSkeleton />}>
+      <Suspense fallback={<div>Loading...</div>}>
         <PostListFilters
           selectedCategory={selectedCategory}
           setSelectedCategory={setSelectedCategory}
