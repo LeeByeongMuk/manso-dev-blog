104d8afa44d13cb3b50d8a3119de431a08cd44f0
LeeByeongMuk <mansogod@gmail.com>
2025-02-16 22:56:09 +0900

    fix: build error

--- src/app/(domain)/(home)/page.tsx
+++ src/app/(domain)/(home)/page.tsx
@@ -1,20 +1,14 @@
 'use client';
 
-import { useState } from 'react';
-
-import PostListFiltersContainer from '@domains/post/list/_components/PostListFiltersContainer';
-
 export default function Home() {
-  const [selectedCategory, setSelectedCategory] = useState('all');
-
   return (
     <section className="mx-auto max-w-3xl bg-background px-6 py-10 text-foreground">
       <h2 className="mb-6 text-3xl font-bold text-foreground">Posts</h2>
 
-      <PostListFiltersContainer
-        selectedCategory={selectedCategory}
-        setSelectedCategory={setSelectedCategory}
-      />
+      {/*<PostListFiltersContainer*/}
+      {/*  selectedCategory={selectedCategory}*/}
+      {/*  setSelectedCategory={setSelectedCategory}*/}
+      {/*/>*/}
 
       {/*<PostListContainer category={selectedCategory} />*/}
     </section>
