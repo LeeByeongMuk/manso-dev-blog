f0b364cdd53ade44a6f3ff97156698edac317cd8
LeeByeongMuk <mansogod@gmail.com>
2025-02-16 23:39:08 +0900

    Update useCatagories.ts

--- src/domains/post/list/_hooks/useCatagories.ts
+++ src/domains/post/list/_hooks/useCatagories.ts
@@ -1,18 +1,13 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 
-// import { BASE_URL } from '@lib/api/url';
+import { BASE_URL } from '@lib/api/url';
 
 export type CategoriesResponse = {
   categories: string[];
 };
 
 async function fetchCategories(): Promise<CategoriesResponse> {
-  const url = `/api/posts/categories`;
-  const res = await fetch(url, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+  const res = await fetch(`${BASE_URL}/api/posts/categories`);
 
   if (!res.ok) {
     throw new Error('Failed to fetch categories');
