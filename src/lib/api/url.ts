ea786e27185fce6b46896b202e4d307b1c86486a
LeeByeongMuk <mansogod@gmail.com>
2025-02-16 22:38:55 +0900

    Update url.ts

--- src/lib/api/url.ts
+++ src/lib/api/url.ts
@@ -3,9 +3,9 @@ function getBaseURL() {
     return `https://${process.env.VERCEL_URL}`;
   }
 
-  // if (process.env.CI) {
-  //   return 'http://127.0.0.1:3000';
-  // }
+  if (process.env.CI) {
+    return '';
+  }
 
   if (process.env.NEXT_PUBLIC_BASE_URL) {
     return process.env.NEXT_PUBLIC_BASE_URL;
