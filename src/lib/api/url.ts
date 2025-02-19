b2ec3591297636f3b7f5ff697d4a0e63026407be
LeeByeongMuk <mansogod@gmail.com>
2025-02-19 23:03:48 +0900

    fix: error

--- src/lib/api/url.ts
+++ src/lib/api/url.ts
@@ -1,13 +1,16 @@
-function getBaseURL() {
-  if (process.env.VERCEL_URL) {
-    return `https://${process.env.VERCEL_URL}`;
-  }
+// function getBaseURL() {
+//   if (process.env.VERCEL_URL) {
+//     return `https://${process.env.VERCEL_URL}`;
+//   }
+//
+//   if (process.env.NEXT_PUBLIC_BASE_URL) {
+//     return process.env.NEXT_PUBLIC_BASE_URL;
+//   }
+//
+//   return 'http://localhost:3000';
+// }
 
-  if (process.env.NEXT_PUBLIC_BASE_URL) {
-    return process.env.NEXT_PUBLIC_BASE_URL;
-  }
-
-  return 'http://localhost:3000';
-}
-
-export const BASE_URL = getBaseURL();
+export const BASE_URL = process.env.VERCEL_URL
+  ? `https://${process.env.VERCEL_URL}`
+  : (process.env.NEXT_PUBLIC_BASE_URL ??
+    (process.env.CI ? 'http://127.0.0.1:3000' : 'http://localhost:3000'));
