b7590f99ad0cc5359aa4bbd4f697407abbe1ddec
LeeByeongMuk <mansogod@gmail.com>
2025-02-19 22:28:19 +0900

    fix: error

--- next.config.mjs
+++ next.config.mjs
@@ -10,13 +10,7 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_BASE_URL:
-      process.env.NEXT_PUBLIC_BASE_URL ??
-      (process.env.VERCEL_ENV === 'preview' && process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : 'http://localhost:3000'),
-  },
+  output: 'standalone',
   webpack: (config, { isServer }) => {
     if (isServer) {
       if (Array.isArray(config.resolve.alias)) {
