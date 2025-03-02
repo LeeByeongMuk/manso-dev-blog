21d64a7ef0c98fde7056626e1259571c9579bd99
LeeByeongMuk <mansogod@gmail.com>
2025-03-02 22:44:10 +0900

    fix: refine env variable handling in server and next.config.js

--- next.config.js
+++ next.config.js
@@ -11,11 +11,7 @@ const nextConfig = {
     ],
   },
   env: {
-    NEXT_PUBLIC_BASE_URL:
-      process.env.NEXT_PUBLIC_BASE_URL ??
-      (process.env.VERCEL_ENV === 'preview' && process.env.VERCEL_URL
-        ? `https://${process.env.VERCEL_URL}`
-        : 'http://localhost:3000'),
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
   },
 };
 
